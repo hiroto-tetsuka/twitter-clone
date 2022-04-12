0224b106f49f24dfbbbc4716317d62f34868ea54
hiroto.tetsuka <hiroto_tetsuka@fandmnet.com>
2022-04-12 00:42:54 +0000

    set timezone

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'MessagesController@index');
+
+Route::resource('messages', 'MessagesController');
